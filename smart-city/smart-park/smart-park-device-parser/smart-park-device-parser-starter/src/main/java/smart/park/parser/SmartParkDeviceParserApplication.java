cde8b2950031cdedaa7be9accd8c8dc1ee8299a3
ljm-cloud <83705498@qq.com>
2022-12-07 13:57:37 +0800

    增加smart-park-device-parser启动模块

---
+++ smart-city/smart-park/smart-park-device-parser/smart-park-device-parser-starter/src/main/java/smart/park/parser/SmartParkDeviceParserApplication.java
@@ -0,0 +1,15 @@
+package smart.park.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ljm
+ * @date 2022/12/7 13:52
+ */
+@SpringBootApplication
+public class SmartParkDeviceParserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkDeviceParserApplication.class,args);
+    }
+}
