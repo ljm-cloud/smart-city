58edd33d6315e58a56f8aa1676aecfa0793f1b69
ljm-cloud <83705498@qq.com>
2022-12-16 18:09:30 +0800

    1.添加Paho Client生成逻辑 2.添加lombok

--- smart-city/smart-device-parser/device-parser-init/src/main/java/smart/parser/init/iot/protocol/IotMsgClientOption.java
+++ smart-city/smart-device-parser/device-parser-init/src/main/java/smart/parser/init/iot/protocol/IotMsgClientOption.java
@@ -1,8 +1,14 @@
 package smart.parser.init.iot.protocol;
 
+import lombok.Data;
+
 /**
  * @author ljm
  * @date 2022/12/5 14:08
  */
+@Data
 public class IotMsgClientOption {
+    private String serverURI;
+    private String userName;
+    private String password;
 }
---
+++ smart-city/smart-device-parser/device-parser-init/src/main/java/smart/parser/init/iot/protocol/mqtt/paho/PahoManager.java
@@ -0,0 +1,51 @@
+package smart.parser.init.iot.protocol.mqtt.paho;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import smart.parser.init.iot.protocol.IotMsgClientOption;
+import smart.parser.init.iot.protocol.IotMsgProtocolManager;
+
+import java.util.Optional;
+
+/**
+ * @author ljm
+ * @date 2022/12/15 13:27
+ */
+@Slf4j
+public class PahoManager implements IotMsgProtocolManager {
+    @Override
+    public Object buildMsgClient(IotMsgClientOption iotMsgClientOption) {
+        PahoClientOption pahoClientOption = (PahoClientOption) iotMsgClientOption;
+        try {
+            MqttAsyncClient client = new MqttAsyncClient(pahoClientOption.getServerURI(),pahoClientOption.getClientId(), Optional.ofNullable(pahoClientOption.getPersistence()).orElseGet(MemoryPersistence::new));
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setUserName(iotMsgClientOption.getUserName());
+            connectOptions.setPassword(iotMsgClientOption.getPassword().toCharArray());
+            connectOptions.setCleanSession(true);
+            client.setCallback(new OnMessageCallback());
+            client.connect(connectOptions);
+            return client;
+        } catch (MqttException e) {
+
+        }
+        return null;
+    }
+    static class OnMessageCallback implements MqttCallback {
+
+        @Override
+        public void connectionLost(Throwable throwable) {
+
+        }
+
+        @Override
+        public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+        }
+    }
+}
