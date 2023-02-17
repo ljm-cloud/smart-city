140f886c466518b7a553a3a3a507d158814f9297
ljm-cloud <83705498@qq.com>
2023-02-17 14:20:18 +0800

    1.引入CloudEvent 2.DeviceParserFunction的parser方法添加参数

--- smart-device-parser/device-parser-functions/src/main/java/smart/parser/functions/DeviceParserFunction.java
+++ smart-device-parser/device-parser-functions/src/main/java/smart/parser/functions/DeviceParserFunction.java
@@ -1,9 +1,11 @@
 package smart.parser.functions;
 
+import io.cloudevents.CloudEvent;
+
 /**
  * @author ljm
  * @date 2022/12/2 18:03
  */
 public interface DeviceParserFunction {
-    void parser();
+    void parser(CloudEvent cloudEvent);
 }
