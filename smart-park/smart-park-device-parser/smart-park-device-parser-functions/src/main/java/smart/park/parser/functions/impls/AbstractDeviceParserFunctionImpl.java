8d24715c8c98f727bf15ef31344f4d0a490bf277
ljm-cloud <83705498@qq.com>
2023-02-17 14:21:48 +0800

    提供对DeviceParserFunction的空实现

---
+++ smart-park/smart-park-device-parser/smart-park-device-parser-functions/src/main/java/smart/park/parser/functions/impls/AbstractDeviceParserFunctionImpl.java
@@ -0,0 +1,11 @@
+package smart.park.parser.functions.impls;
+
+import smart.parser.functions.DeviceParserFunction;
+
+/**
+ * @author ljm
+ * @date 2023/2/9 17:28
+ */
+public abstract class AbstractDeviceParserFunctionImpl implements DeviceParserFunction {
+    
+}
---
+++ smart-park/smart-park-device-parser/smart-park-device-parser-functions/src/main/java/smart/park/parser/functions/impls/aq/AqParserFunctionImpl.java
@@ -0,0 +1,16 @@
+package smart.park.parser.functions.impls.aq;
+
+import io.cloudevents.CloudEvent;
+import smart.park.parser.functions.impls.AbstractDeviceParserFunctionImpl;
+
+/**
+ * @author ljm
+ * @date 2023/2/17 11:48
+ */
+public class AqParserFunctionImpl extends AbstractDeviceParserFunctionImpl {
+
+    @Override
+    public void parser(CloudEvent cloudEvent) {
+
+    }
+}
