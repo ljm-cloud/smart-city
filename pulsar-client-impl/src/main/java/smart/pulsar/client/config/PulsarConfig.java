95b9f4913721f600b79baa43587f679551db5ced
ljm-cloud <83705498@qq.com>
2023-02-24 17:11:52 +0800

    添加Pulsar配置项

---
+++ pulsar-client-impl/src/main/java/smart/pulsar/client/config/PulsarConfig.java
@@ -0,0 +1,19 @@
+package smart.pulsar.client.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author ljm
+ * @date 2023/2/24 14:41
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "smart.pulsar")
+public class PulsarConfig {
+    private Map<String,PulsarPublisherConfig> publishers = Collections.emptyMap();
+}
