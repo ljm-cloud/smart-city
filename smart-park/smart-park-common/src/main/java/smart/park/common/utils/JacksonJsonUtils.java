5b87388994c2089517d8e123af05b5712be3273a
ljm-cloud <83705498@qq.com>
2023-03-15 16:07:10 +0800

    引入com.alibaba.fastjson 并封装fastjson工具类与Jackson工具类

---
+++ smart-park/smart-park-common/src/main/java/smart/park/common/utils/JacksonJsonUtils.java
@@ -0,0 +1,53 @@
+package smart.park.common.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonJsonUtils{
+
+    public static JavaType getCollectionType(Class<?> collectionClass, Class<?>... elementClasses) {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
+    }
+
+    public static <T> T jsonToList(String json, TypeReference<T> valueType) {
+
+        if (json == null) {
+            return null;
+        }
+
+        json = json.trim();
+        if (json.equals("")) {
+            return null;
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(json, valueType);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static <T> T jsonToBean(String json,Class<T> valueType){
+        if (json==null){
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(json, valueType);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String beanToJson(Object value){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(value);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
