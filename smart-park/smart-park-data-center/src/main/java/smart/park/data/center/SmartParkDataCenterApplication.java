179556ed10ce8902e0513d70101701e9dfbcc8a1
ljm-cloud <83705498@qq.com>
2023-03-16 17:54:38 +0800

    添加数据中心启动类

---
+++ smart-park/smart-park-data-center/src/main/java/smart/park/data/center/SmartParkDataCenterApplication.java
@@ -0,0 +1,15 @@
+package smart.park.data.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ljm
+ * @date 2023/3/16 17:53
+ */
+@SpringBootApplication
+public class SmartParkDataCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkDataCenterApplication.class,args);
+    }
+}
