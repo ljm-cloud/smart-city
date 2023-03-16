ec212eab09473f61ad94b79fad04acec18a78921
ljm-cloud <83705498@qq.com>
2023-03-16 17:28:22 +0800

    将AqParser交给spring管理

--- smart-park/smart-park-device-parser/smart-park-device-parser-init/src/main/java/smart/park/parser/init/parser/AqParser.java
+++ smart-park/smart-park-device-parser/smart-park-device-parser-init/src/main/java/smart/park/parser/init/parser/AqParser.java
@@ -1,9 +1,12 @@
 package smart.park.parser.init.parser;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author ljm
  * @date 2023/3/15 18:15
  */
+@Service
 public class AqParser extends AbstractParser{
     @Override
     public String getParserTopicInput() {
