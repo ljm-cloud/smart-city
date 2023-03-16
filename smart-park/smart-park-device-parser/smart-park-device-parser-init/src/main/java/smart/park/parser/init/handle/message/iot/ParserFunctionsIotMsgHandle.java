b9a79dd263819a54abe47d1bd2bc187610c51f67
ljm-cloud <83705498@qq.com>
2023-03-16 17:20:28 +0800

    添加ParserFunctionsIotMsgHandle处理器处理接收到的IOT Messages

--- smart-park/smart-park-device-parser/smart-park-device-parser-init/src/main/java/smart/park/parser/init/handle/message/iot/ParserFunctionsIotMsgHandle.java
+++ smart-park/smart-park-device-parser/smart-park-device-parser-init/src/main/java/smart/park/parser/init/handle/message/iot/ParserFunctionsIotMsgHandle.java
@@ -1,6 +1,13 @@
 package smart.park.parser.init.handle.message.iot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import smart.park.common.provider.SmartParkApplicationContextProvider;
+import smart.park.common.utils.FastJsonUtil;
+import smart.park.parser.init.common.dto.TrashDataVo;
+import smart.park.parser.init.common.helper.GlobalHelper;
+import smart.park.parser.init.service.ParserService;
+import java.util.Optional;
 
 /**
  * @author ljm
@@ -8,6 +15,7 @@ import org.springframework.stereotype.Component;
  * 以Pulsar Functions的方式解析数据解析器
  */
 @Component
+@Slf4j
 public class ParserFunctionsIotMsgHandle extends AbsIotMessageHandle {
     @Override
     public String handleType() {
@@ -16,6 +24,19 @@ public class ParserFunctionsIotMsgHandle extends AbsIotMessageHandle {
 
     @Override
     public void handle(String topic,String message,String consumerName) {
-
+        TrashDataVo trashDataVo;
+        try {
+            trashDataVo = FastJsonUtil.jsonToBean(message, TrashDataVo.class);
+            if (trashDataVo==null){
+                return;
+            }
+        }catch (Exception e){
+            log.error("ParserFunctionsIotMsgHandle.handle.messageToTrashDataVo error|"+message,e);
+            return;
+        }
+        //放入Pulsar Functions Topic Input
+        Optional.ofNullable(GlobalHelper.abstractParserMap.get(trashDataVo.getFPort())).ifPresent(abstractParser -> {
+            SmartParkApplicationContextProvider.getBean(ParserService.class).publishParserFunctions(abstractParser.getParserTopicInput(),trashDataVo);
+        });
     }
 }
