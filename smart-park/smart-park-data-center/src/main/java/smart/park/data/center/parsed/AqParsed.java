087a8deb0fd9d1267d7f79dfc41b1e1ac9c129f8
ljm-cloud <83705498@qq.com>
2023-03-21 10:02:13 +0800

    将AqParsed注入spring容器

--- smart-park/smart-park-data-center/src/main/java/smart/park/data/center/parsed/AqParsed.java
+++ smart-park/smart-park-data-center/src/main/java/smart/park/data/center/parsed/AqParsed.java
@@ -1,5 +1,6 @@
 package smart.park.data.center.parsed;
 
+import org.springframework.stereotype.Service;
 import smart.park.common.config.DeviceType;
 import smart.park.data.center.parsed.base.AbstractParsed;
 
@@ -7,6 +8,7 @@ import smart.park.data.center.parsed.base.AbstractParsed;
  * @author ljm
  * @date 2023/3/16 17:58
  */
+@Service
 public class AqParsed extends AbstractParsed {
     @Override
     public DeviceType getDeviceType() {
