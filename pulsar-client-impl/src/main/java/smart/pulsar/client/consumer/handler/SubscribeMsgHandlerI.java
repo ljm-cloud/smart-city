666560d6b4dc4cee45cacfae2afcbadfa9a34573
ljm-cloud <83705498@qq.com>
2023-03-23 15:54:50 +0800

    为SubscribeMsgHandlerI接口添加注释

--- pulsar-client-impl/src/main/java/smart/pulsar/client/consumer/handler/SubscribeMsgHandlerI.java
+++ pulsar-client-impl/src/main/java/smart/pulsar/client/consumer/handler/SubscribeMsgHandlerI.java
@@ -3,6 +3,7 @@ package smart.pulsar.client.consumer.handler;
 /**
  * @author ljm
  * @date 2023/3/23 15:36
+ * 实现这个接口可以自定义处理逻辑处理(handle())指定Topic(getTopic())的消息datas
  */
 public interface SubscribeMsgHandlerI {
     void handle(byte[] datas);
