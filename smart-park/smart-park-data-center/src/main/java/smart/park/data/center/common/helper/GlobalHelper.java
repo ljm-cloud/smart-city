025291198d3c2bca59af1d126a3e432f785150b0
ljm-cloud <83705498@qq.com>
2023-03-23 14:58:58 +0800

    添加AbstractParsed的映射关系，并初始化

---
+++ smart-park/smart-park-data-center/src/main/java/smart/park/data/center/common/helper/GlobalHelper.java
@@ -0,0 +1,14 @@
+package smart.park.data.center.common.helper;
+
+import smart.park.data.center.parsed.base.AbstractParsed;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author ljm
+ * @date 2023/3/23 14:45
+ */
+public class GlobalHelper {
+    public static Map<Integer, AbstractParsed> abstractParsedMap = Collections.emptyMap();
+}
--- smart-park/smart-park-data-center/src/main/java/smart/park/data/center/listener/ApplicationReadyEventListener.java
+++ smart-park/smart-park-data-center/src/main/java/smart/park/data/center/listener/ApplicationReadyEventListener.java
@@ -5,8 +5,12 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import smart.park.common.provider.SmartParkApplicationContextProvider;
+import smart.park.data.center.common.helper.GlobalHelper;
+import smart.park.data.center.parsed.base.AbstractParsed;
 import smart.pulsar.client.service.PulsarConsumerService;
 import smart.pulsar.client.service.PulsarPublisherService;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author ljm
@@ -19,6 +23,7 @@ public class ApplicationReadyEventListener implements ApplicationListener<Applic
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         log.info("smart-park-data-center applicationReadyEvent start...");
+        GlobalHelper.abstractParsedMap = SmartParkApplicationContextProvider.getApplicationContext().getBeansOfType(AbstractParsed.class).values().stream().collect(Collectors.toMap(abstractParsed -> abstractParsed.getDeviceType().getValue(), Function.identity()));
         SmartParkApplicationContextProvider.getBean(PulsarPublisherService.class).initPublishers();
         SmartParkApplicationContextProvider.getBean(PulsarConsumerService.class).initConsumers();
         log.info("smart-park-data-center applicationReadyEvent end...");
